f2c36819395f4e7912c136dcdac89ba707f77803
Ezewuzie Okafor <eokafor@seamfix.com>
2016-12-07 09:50:42 +0100

    removed commented out code

--- ims-app-core/src/main/java/com/kyc/incentives/appcore/incentivesimpl/DirtyRecord.java
+++ ims-app-core/src/main/java/com/kyc/incentives/appcore/incentivesimpl/DirtyRecord.java
@@ -15,32 +15,8 @@ import com.sf.biocapture.entity.enums.StatusType;
 public class DirtyRecord extends CleanRecord {
 	
 	private final List<StatusType> targetStatusTypes = Arrays.asList( new StatusType[]{ StatusType.FAILED } );
-
-//	public DirtyRecord() {
-//		super(CleanRecordService.getInstance());
-//	}
-//
-//	@Override
-//	protected long getCount(IncentiveUserTriggerHistory incentiveUserTriggerHistory) {
-//		Date startDate = incentiveUserTriggerHistory.getStartDate();
-//		Date endDate = incentiveUserTriggerHistory.getEndDate();
-//		
-//		AppUser user = incentiveUserTriggerHistory.getUser();
-//		
-//		String email = user.getEmail();
-//		
-//		Long cleanRecordCount = CleanRecordService.getInstance().getCleanRecordDealerCount(email, startDate, endDate, targetStatusTypes);
-//		
-//		if(cleanRecordCount == null){
-//			return 0;
-//		}
-//		
-//		return cleanRecordCount;
-//	}
 	
 	protected List<StatusType> getTargetStatusTypes(){
 		return targetStatusTypes;
 	}
-
-	
 }
